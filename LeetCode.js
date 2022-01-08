75704d46287a439565923db50ae73e0e8d7e2c6b
Dillon Coleman <coleman399@gmail.com>
2022-01-07 19:29:51 -0700

    revisited twoSums

--- LeetCode.js
+++ LeetCode.js
@@ -19,27 +19,48 @@
 // output = [0, 1]
 
 
-const test_case = "[]" 
+//twoSum revisited
 
-const isValid = (s) => {
-    let map = {
-        '(':')',
-        '{':'}',
-        '[':']',
-    }
-    let stack = [];
-    for (let char of s) {
-        if(map[char]) {
-            stack.push(map[char]);
-        } else if (char !== stack.pop()){
-            return false;
+let nums = [3,2,3]
+let target = 6
+
+const twoSum = (nums, target) => {
+    const previousValues = {};
+    for (let i = 0; i < nums.length; i++){
+        const currentNum = nums[i]; // storing current value
+        const neededValue = target - currentNum; // getting neededValue
+        const index2 = previousValues[neededValue]; // Calling previousValues using key "neededValue" and returning its value which is the index of the neededValue 
+        if (index2 != null){ // if neededValue is not in previousValues, index2 is undefined
+            console.log([index2, i]); //returns indices
+        }else{
+            previousValues[currentNum] = i; //adds current number to previousValues as a Key and i as its value
         }
     }
-    if (stack.length === 0) {
-        return true;
-    } else {
-        return false;
-    }
-};
+}
 
-console.log(isValid(test_case))
+twoSum(nums, target);
+
+// const test_case = "[]" 
+
+// const isValid = (s) => {
+//     let map = {
+//         '(':')',
+//         '{':'}',
+//         '[':']',
+//     }
+//     let stack = [];
+//     for (let char of s) {
+//         if(map[char]) {
+//             stack.push(map[char]);
+//         } else if (char !== stack.pop()){
+//             return false;
+//         }
+//     }
+//     if (stack.length === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+
+// console.log(isValid(test_case))
